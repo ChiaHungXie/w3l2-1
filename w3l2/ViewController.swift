f3d926fb38cd44e0493efcea6eabe37b2bacaebc
Danny Shen <shenfive@gmail.com>
2019-07-20 14:58:11 +0800

    first ver

--- w3l2/ViewController.swift
+++ w3l2/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var counter = 0.0
+    var timer:Timer!
+    var speed = 2.0
+    @IBOutlet var segment: UISegmentedControl!
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let newView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        newView.center = self.view.center
+        newView.backgroundColor = UIColor.red
+        newView.transform = CGAffineTransform(rotationAngle: 0)
+        view.addSubview(newView)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: { (t) in
+            self.rotateView(targateView: newView)
+        })
     }
-
-
+    func rotateView(targateView:UIView){
+        let angle =  counter * Double.pi / 180
+        targateView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        counter += speed
+    }
+    @IBAction func sliderAction(_ sender: UISlider) {
+        speed = Double(sender.value)
+    }
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        
+    }
+    
 }
 
